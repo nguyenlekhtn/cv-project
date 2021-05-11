5b32afa288309d0833bd2eb19e69d8842b5ea831
nguyenlekhtn <nguyenlekhtn@gmail.com>
2021-05-11 09:25:51 +0700

    Init form component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import Form from "./components/Form";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      fullName: "",
+      email: "",
+      phoneNumber: "",
+      schoolName: "",
+      titleOfStudy: "",
+      dateOfStudy: "",
+      company: "",
+      position: "",
+      mainTask: "",
+      dateStart: "",
+      dateEnd: "",
+    };
+  }
+
+  handleChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  getData = (arr) => {
+    const generalData = {};
+    arr.forEach((item) => (generalData[item] = this.state[item]));
+    return generalData;
+  };
+
+  render() {
+    // const generalData = this.getData(["fullName", "email", "phoneNumber"]);
+    return (
+      <div>
+        <Form />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/InputData.js
@@ -0,0 +1,64 @@
+const InputData = [
+  {
+    title: "fullName",
+    type: "text",
+    description: "Name",
+  },
+  {
+    title: "email",
+    type: "email",
+    description: "Email",
+  },
+  {
+    title: "phoneNumber",
+    type: "phone",
+    description: "Phone number",
+  },
+  {
+    title: "schoolName",
+    type: "text",
+    description: "School name",
+  },
+  {
+    title: "titleOfStudy",
+    type: "text",
+    description: "Title of study",
+  },
+  {
+    title: "studyStart",
+    type: "number",
+    description: "Start year of study",
+  },
+  {
+    title: "studyEnd",
+    type: "number",
+    description: "End year of study",
+  },
+  {
+    title: "company",
+    type: "text",
+    description: "Company name",
+  },
+  {
+    title: "postion",
+    type: "text",
+    description: "Position",
+  },
+  {
+    title: "mainTask",
+    type: "text",
+    description: "Main Task of Job",
+  },
+  {
+    title: "jobStart",
+    type: "date",
+    description: "Start Date of Job",
+  },
+  {
+    title: "jobEnd",
+    type: "date",
+    description: "End Date of Job",
+  },
+];
+
+export default InputData;
